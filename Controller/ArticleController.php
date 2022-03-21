87cc5228406c208858725ea3becf205dc0163f48
unknown <cskeith01@gmail.com>
2022-03-21 12:30:07 +0100

    WIP fetching artical Data

--- Controller/ArticleController.php
+++ Controller/ArticleController.php
@@ -4,6 +4,14 @@ declare(strict_types = 1);
 
 class ArticleController
 {
+    private DatabaseManager $databaseManager;
+
+    // This class needs a database connection to function
+    public function __construct(DatabaseManager $databaseManager)
+    {
+        $this->databaseManager = $databaseManager;
+    }
+
     public function index()
     {
         // Load all required data
@@ -15,15 +23,21 @@ class ArticleController
     }
 
     // Note: this function can also be used in a repository - the choice is yours
-    private function getArticles()
+    private function getArticles(): PDOStatement
     {
+        $rawArticles = [];
         // TODO: prepare the database connection
+        $rawArticle = $this->databaseManager->connection->query
+        (
+            "SELECT * FROM articles "
+        );
+        $rawArticle->fetch();
+        return $rawArticle;
         
         // Note: you might want to use a re-usable databaseManager class - the choice is yours
         
         // TODO: fetch all articles as $rawArticles (as a simple array)
-        $rawArticles = [];
-
+        
         $articles = [];
         foreach ($rawArticles as $rawArticle) {
             // We are converting an article from a "dumb" array to a much more flexible class
