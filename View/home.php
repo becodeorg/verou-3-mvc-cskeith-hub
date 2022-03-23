04be95bf4625ca41f5e0b3fe7c065f4949e15cba
unknown <cskeith01@gmail.com>
2022-03-23 20:38:10 +0100

    Add some home page Content

--- View/home.php
+++ View/home.php
@@ -3,7 +3,9 @@
 <?php // Use any data loaded in the controller here ?>
 
 <section>
-    <p>Put your content here.</p>
+    <h1>Welcome To MVC exercise Site of Colin Renkema</h1>
+    <p>Here I learn about Model,View and Controller.</p>
+    <p>Also getting more in touch with PHP and the SQL language.</p>
 </section>
 
 <?php require 'View/includes/footer.php'?>
